cd295dd84859edab27235415427cfbff286e96c2
xietao <xietao@dp2003.com>
2019-04-05 15:28:40 +0800

    修改 .Core Project

---
+++ DigitalPlatform.Core/LibraryManager.cs
@@ -0,0 +1,15 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalPlatform.Core
+{
+    /// <summary>
+    /// chord 项目内所有函数库的共同全局参数
+    /// </summary>
+    public static class LibraryManager
+    {
+        public static ILog Log { get; set; }
+    }
+}
