d2f05f4ac583848d4d3f0e0f8f2032bc7089ab24
xietao <xietao@dp2003.com>
2019-04-13 11:55:53 +0800

    为了 Marc8Encoding 添加一系列代码

---
+++ DigitalPlatform.Core/XML/XmlExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DigitalPlatform.Xml
+{
+    public static class ElementExtension
+    {
+        public static string GetElementText(this XmlElement nodeRoot,
+    string strXpath)
+        {
+            XmlNode node = nodeRoot.SelectSingleNode(strXpath);
+            if (node == null)
+                return "";
+
+            XmlNode nodeText;
+            nodeText = node.SelectSingleNode("text()");
+
+            if (nodeText == null)
+                return "";
+            else
+                return nodeText.Value;
+        }
+    }
+}
