a18be4321c41e0e058cfcfa3d21ff9830b7b12e2
xietao <xietao@dp2003.com>
2019-05-13 11:48:17 +0800

    完善 IssnInsertHyphen()

--- DigitalPlatform.Core/IsbnSplitter.cs
+++ DigitalPlatform.Core/IsbnSplitter.cs
@@ -638,8 +638,15 @@ namespace DigitalPlatform.Script
                 return -1;
             }
 
+
             strSource = strSource.Replace("-", "");
             strSource = strSource.Replace(" ", "");
+
+            if (bForce8 && strSource.StartsWith("977") == false)
+            {
+                strError = $"'{strISSN}' 不是 ISSN 条码号(注: ISSN 条码号应该以 '977' 开头)";
+                return -1;
+            }
 
             bool bAdjustLength = false; // 是否调整过输入的strISSN的长度
 
